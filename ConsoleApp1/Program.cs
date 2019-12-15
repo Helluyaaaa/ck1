49d71b583105a17e75b4eca7e92a6760e03ed9b5
凯夫 邓 <943514827@qq.com>
2019-12-15 23:17:02 +0800

    上传了最后的波纹

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test;
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+        public static string connectionString = "Data Source=.;Server=(Local);Integrated Security=true;";
+        static void Main(string[] args)
+        {
+            using(SqlConnection connection=new SqlConnection(connectionString))
+            {
+                connection.Open();
+            }
+
+        }
+    }
+}
---
+++ test/ServerPersonal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class ServerPersonal
+    {
+        public int ID { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+        public string ServerType { get; set; }
+        public string Area { get; set; }
+        public int Phone { get; set; }
+        public string SelfIntroduction { get; set; }
+        public string State { get; set; }
+    }
+}
